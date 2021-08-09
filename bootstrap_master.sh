29e15735ad817dbd5dd8ef3c67be7bc5f66e1b97
Nikhil M <nikhil.m@yellowmessenger.com>
2021-08-10 00:07:24 +0530

    Expose kubeconfig as a download from the master

--- bootstrap_master.sh
+++ bootstrap_master.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sudo apt install nginx -y
-
 # Initialize Kubernetes
 echo "[TASK 1] Initialize Kubernetes Cluster"
 kubeadm init --upload-certs --apiserver-advertise-address=192.168.0.200 --pod-network-cidr=10.254.0.0/16 >> /root/kubeinit.log 2>/dev/null
@@ -10,6 +8,7 @@ kubeadm init --upload-certs --apiserver-advertise-address=192.168.0.200 --pod-ne
 echo "[TASK 2] Copy kube admin config to Vagrant user .kube directory"
 mkdir /home/vagrant/.kube
 cp /etc/kubernetes/admin.conf /home/vagrant/.kube/config
+cp /etc/kubernetes/admin.conf /var/www/html/admin.conf
 chown -R vagrant:vagrant /home/vagrant/.kube
 
 # Deploy overlay network
@@ -21,3 +20,35 @@ su - vagrant -c "kubectl apply -f https://docs.projectcalico.org/manifests/calic
 echo "[TASK 4] Generate and save cluster join command to /joincluster.sh"
 sudo kubeadm token create --print-join-command --ttl 0
 echo "sudo kubeadm join $(sudo cat /etc/kubernetes/kubelet.conf | grep server | cut -d '/' -f 3) --token $(sudo kubeadm token list | grep forever | cut -c 1-23) --discovery-token-ca-cert-hash sha256:$(openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //')" > /var/www/html/index.nginx-debian.html
+
+echo "[TASK 5] Exporting Kubeconfig file through nginx on master node ip"
+
+sudo apt install nginx -y
+cat <<EOF | sudo tee /var/www/html/config.html
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Add icon library -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+.btn {
+  background-color: DodgerBlue;
+  border: none;
+  color: white;
+  padding: 12px 30px;
+  cursor: pointer;
+  font-size: 20px;
+}
+/* Darker background on mouse-over */
+.btn:hover {
+  background-color: RoyalBlue;
+}
+</style>
+</head>
+<body>
+<h2>Download devcluster Kubeconfig file</h2>
+<a href="admin.conf" download="devcluster-k8s.yaml" download><button class="btn"><i class="fa fa-download"></i> Download</button></a>
+</body>
+</html>
+EOF
