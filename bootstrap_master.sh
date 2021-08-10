b02945c7a7ef842e07334685a814e2dea06870df
Nikhil M <nikhil.m@yellowmessenger.com>
2021-08-10 14:40:32 +0530

    Fix commands to expose join command and conf

--- bootstrap_master.sh
+++ bootstrap_master.sh
@@ -9,7 +9,6 @@ echo "########## Starting controlplane setup on $HOSTNAME ##########"
 echo "[CONTROLPLANE TASK 2] Copy kube admin config to Vagrant user .kube directory"
 mkdir /home/vagrant/.kube
 cp /etc/kubernetes/admin.conf /home/vagrant/.kube/config
-cp /etc/kubernetes/admin.conf /var/www/html/admin.conf
 chown -R vagrant:vagrant /home/vagrant/.kube
 
 # Deploy overlay network
@@ -20,12 +19,12 @@ su - vagrant -c "kubectl apply -f https://docs.projectcalico.org/manifests/calic
 # Generate Cluster join command
 echo "[CONTROLPLANE TASK 4] Generate and save cluster join command to /joincluster.sh"
 sudo kubeadm token create --print-join-command --ttl 0
-echo "sudo kubeadm join $(sudo cat /etc/kubernetes/kubelet.conf | grep server | cut -d '/' -f 3) --token $(sudo kubeadm token list | grep forever | cut -c 1-23) --discovery-token-ca-cert-hash sha256:$(openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //')" > /var/www/html/index.nginx-debian.html
 
 echo "[CONTROLPLANE TASK 5] Exporting Kubeconfig file through nginx on master node ip"
 
+
 sudo apt install nginx -y
-cat <<EOF | sudo tee /var/www/html/config.html
+cat <<EOF | sudo tee /var/www/html/index.nginx-debian.html
 <!DOCTYPE html>
 <html>
 <head>
@@ -53,4 +52,14 @@ cat <<EOF | sudo tee /var/www/html/config.html
 </body>
 </html>
 EOF
+
+echo "Copying the admin.conf to be available at the path /admin.conf"
+sudo cp /etc/kubernetes/admin.conf /var/www/html/admin.conf
+
+echo "Adding the join command at /join path"
+echo "sudo kubeadm join $(sudo cat /etc/kubernetes/kubelet.conf | grep server | cut -d '/' -f 3) --token $(sudo kubeadm token list | grep forever | cut -c 1-23) --discovery-token-ca-cert-hash sha256:$(openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //')" | sudo tee /var/www/html/join
+
+echo "Setting 644 file permissions for all files in /var/www/html dir"
+sudo chmod 644 /var/www/html/*
+
 echo "########## Completed controlplane setup on $HOSTNAME ##########"
