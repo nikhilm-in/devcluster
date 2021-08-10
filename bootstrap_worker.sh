d2bf4a56e949e55bd2f51a6c04e9d64aca63692f
Nikhil M <nikhil.m@yellowmessenger.com>
2021-08-10 14:41:01 +0530

    Changed path for fetching join command from master

--- bootstrap_worker.sh
+++ bootstrap_worker.sh
@@ -2,5 +2,5 @@
 echo "########## Joining $HOSTNAME to the cluster ##########"
 # Join worker nodes to the Kubernetes cluster
 echo "[DATAPLANE TASK 1] Join node to Kubernetes Cluster"
-curl -s GET "node0" | bash 
+curl -s GET "http://node0/join" | bash 
 echo "########## Joined $HOSTNAME to the cluster ##########"
